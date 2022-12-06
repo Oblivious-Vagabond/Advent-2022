dd357d1e59fcaf500c5732ebb25c234f15e93209
Oblivious-Vagabond <photxx@gmail.com>
2022-12-06 06:58:00 +0200

    adjust day 5

--- Day5P1.py
+++ Day5P1.py
@@ -1,21 +1,6 @@
 with open('Day5/test.txt') as f:
     lines = f.readlines()
 
-# f = 0
-# stacks = int(len(lines[0])/4)
-# stack = [[] for i in range(stacks)]
-# for i,l in enumerate(lines):
-#     if (f==0):
-#         if(lines[i+1]=='\n'):
-#             f=1
-#             continue
-#         for sta,b in enumerate([l[s*4+1] for s in range(stacks)]):
-#             if b != ' ': stack[sta].insert(0,b)
-#     else:
-#         if(l=='\n'): continue
-#         [num,s,e] = [int(n) for n in l.strip('\n').split()[1::2]]
-#         for n in range(num):
-#             stack[e-1].append(stack[s-1].pop())
 start = 0
 stacks = int(len(lines[0])/4)
 stack = [[] for i in range(stacks)]
@@ -30,6 +15,7 @@ for l in lines[start:]:
     [num,s,e] = [int(n) for n in l.strip('\n').split()[1::2]]
     for n in range(num):
         stack[e-1].append(stack[s-1].pop())
+
 out = ''
 for s in stack:
     out += s.pop()
